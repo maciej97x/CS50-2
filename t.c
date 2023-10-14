548bf20d08866fa4c01be908d4637aa56c06c3eb
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-14 21:45:37 +0000

    Sat, Oct 14, 2023, 9:45 PM +00:00

---
+++ t.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+
+{
+    FILE *file = fopen("Data ")
+
+
+
+   char *s = get_string("s: ");
+
+   char *t = malloc(strlen(s) + 1);
+
+   for (int i = 0; i < strlen(s) + 1; i++)
+
+   printf("s: %s\n", s);
+   printf("t: %s\n", t);
+
+
+
+}
+
+
