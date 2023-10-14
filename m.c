307882fa78f76d6a9a1af7e18a55f24a3e9bec4a
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-14 09:38:47 +0000

    Sat, Oct 14, 2023, 9:38 AM +00:00

--- m.c
+++ m.c
@@ -7,4 +7,19 @@ int main(void)
    int i = get_int("i: ");
    int j = get_int("j: ");
 
+
+   if (i == j)
+
+   {
+    printf("Same \n");
+   }
+
+   else
+
+   {
+    printf("Different \n");
+
+   }
+
+
 }
