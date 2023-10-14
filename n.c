2427418685103cbbfbb715dbfcb7d29129a0623c
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-14 09:49:20 +0000

    Sat, Oct 14, 2023, 9:49 AM +00:00

---
+++ n.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+
+{
+   string t = get_string("t ");
+   string j = get_string("j: ");
+
+
+
+    printf("t\n", t);
+    printf("j\n", j);
+
+
+   }
+
+
+}
