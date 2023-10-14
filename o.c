8707643a66bc75492d3d640bc49ebcb6cc931446
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-14 21:11:50 +0000

    Sat, Oct 14, 2023, 9:11 PM +00:00

---
+++ o.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+
+{
+   char *s = get_string("s: ");
+
+   char *t = s;
+
+   t [0] = toupper(t[0]);
+
+   printf("s: %s\n", s);
+
+
+}
+
+
