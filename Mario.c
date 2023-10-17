63a0b467cabe9f130df5c7804c50c030b76ae5dc
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-17 17:04:34 +0000

    Tue, Oct 17, 2023, 5:04 PM +00:00

---
+++ Mario.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = 0;
+    do
+    {
+        height = get_int("Numbes of rows: ");
+    }
+    while (height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+    printf("#");
+    }
+}
--- u.c
+++ u.c
@@ -1,17 +1,20 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int height = 0;
     do
     {
-        height = get_int("Number ");
+        height = get_int("Height: ");
     }
     while (height < 1);
 
-    for (int = 0; int < height; i++)
-    {
-    printf("#");
-    }
+    for (int i = 0; i < height; i ++)
+
+        {
+            printf("#");
+
+            //k adds the left hand spaces in the pyramid
+        }
 }
