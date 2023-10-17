6c83c8380a5f9548e70d793b40d2ba7bbbb21c3b
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-17 09:31:05 +0000

    Tue, Oct 17, 2023, 9:31 AM +00:00

--- population.c
+++ population.c
@@ -4,18 +4,15 @@
 int main(void)
 {
   int start;
-  do
   {
     start = get_int("Start size: ");
   }
-  while (start < 9);
 
   int end;
-  do
+
   {
     end = get_int("End size: ");
   }
-  while (end < start);
 
   int years = 0;
 
