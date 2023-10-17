3ec642817acc2b263c5bfcf2c5389dfa918b6745
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-17 16:33:25 +0000

    Tue, Oct 17, 2023, 4:33 PM +00:00

--- u.c
+++ u.c
@@ -1,3 +1,15 @@
-mkdir population
-cd population
-pupulation/ $
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Number ");
+    }
+    while (height < 1);
+
+    for (i = 0; i < height; i++)
+    printf("# \n");
+}
