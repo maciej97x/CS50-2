c21c7df6e9ac6228595520b7f2d2649e09b9b170
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-17 09:15:22 +0000

    Tue, Oct 17, 2023, 9:15 AM +00:00

--- population.c
+++ population.c
@@ -4,11 +4,10 @@
 int main(void)
 {
     // TODO: Prompt for start size
-
-    int born = get_int("Population of Llamas: ");
+    int start = get_int("Start population of Llamas: ");
     // TODO: Prompt for end size
-
+    int end = get_int("End population of Llamas: ");
     // TODO: Calculate number of years until we reach threshold
-
+    
     // TODO: Print number of years
 }
