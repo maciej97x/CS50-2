1796e3829edbc53d9ef98216b0dce06d63067024
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-17 17:12:51 +0000

    Tue, Oct 17, 2023, 5:12 PM +00:00

--- mario.c
+++ mario.c
@@ -13,5 +13,7 @@ int main(void)
     for (int i = 0; i < height; i++)
     {
     printf("#");
+    
     }
+
 }
--- u.c
+++ u.c
@@ -4,17 +4,28 @@
 int main(void)
 {
     int height = 0;
+
+    //Asks the user to provide a number between 1 nad 8 inclusive
     do
     {
         height = get_int("Height: ");
     }
-    while (height < 1);
+    while (height > 8 || height < 1);
 
     for (int i = 0; i < height; i ++)
-
+    {
+        for (int k = 1; k < height - i; k++)
         {
-            printf("#");
+            printf("*");
 
             //k adds the left hand spaces in the pyramid
         }
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+            //i and j loops pint our the right hand pyramid
+        }
+
+        printf("\n");
+    }
 }
