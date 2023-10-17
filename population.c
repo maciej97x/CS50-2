db52db1b0a966b782ed0d3e8059f682be55a8a73
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-17 09:19:27 +0000

    Tue, Oct 17, 2023, 9:19 AM +00:00

--- population.c
+++ population.c
@@ -3,11 +3,10 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
     int start = get_int("Start size: ");
-    // TODO: Prompt for end size
-    int end = get_int("End size: ");
-    // TODO: Calculate number of years until we reach threshold
 
-    // TODO: Print number of years
+    int end = get_int("End size: ");
+
+
+  
 }
