68a11facf9fb0a70f90e8e205767a0a363b49f54
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 10:38:54 +0000

    Wed, Oct 18, 2023, 10:38 AM +00:00

--- test.c
+++ test.c
@@ -3,34 +3,26 @@
 
 int main(void)
 {
-    int height;
+    int start;
     do
     {
-        height = get_int("Numbers of rows ");
+        start = get_int("Start ");
     }
-    while (height < 1 || height > 8);
+    while (start < 1);
 
-    for (int i = 0; i < height; i++)
+    int end;
+    do
     {
-        for (int space = height - 1; space > i; space--)
-        {
-            printf(" ");
-        }
-        for (int hash = 0; hash <= i; hash++)
-        {
-            printf("#");
-        }
-        printf(" ");
-         for (int hash2 = 0; hash2 <= i; hash2++)
-        {
-            printf("#");
-        }
-          printf(" ");
-         for (int hash3 = 0; hash3 <= i; hash3++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        end = get_int("End ");
     }
+    while (end > 8);
+
+    int years = 0;
+    do
+    {
+        start = start + start / 3 - start / 4;
+        years++;
+    }
+    while (start < end);
+    printf("Years %i", years);
 }
