22b37149a7173175b467768bf13bf2b911a6e3df
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 06:45:38 +0000

    Wed, Oct 18, 2023, 6:45 AM +00:00

--- mario2.c
+++ mario2.c
@@ -13,6 +13,7 @@ int main(void)
     for (int i = 0; i < height; i++)
     {
         for (int spaces = height - 1; spaces > i; spaces--)
+        else 
         {
             printf(" ");
         }
