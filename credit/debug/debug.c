46c24e80d7e1089f2c2582b8aecd8b857e4fa111
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 08:45:16 +0000

    Wed, Oct 18, 2023, 8:45 AM +00:00

--- credit/debug/debug.c
+++ credit/debug/debug.c
@@ -2,13 +2,18 @@
 // Learn to debug buggy code
 
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     // Ask for your name and where live
-    name = get_string("What is your name? ")
-    location = get_string("Where do you live? ")
+    string name = get_string("What is your name? ");
+    string location = get_string("Where do you live? ");
 
     // Say hello
-    print("Hello, %i, from %i!", name, location)
+
+    printf("Hello, %s, from %s!\n", name, location);
+
+
 }
+
