530da687d2f6c21eeef8c545304852625a6a5b88
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 17:52:04 +0000

    Wed, Oct 18, 2023, 5:52 PM +00:00

--- test.c
+++ test.c
@@ -22,11 +22,24 @@ int main(void)
         }
         printf(" ");
 
-          for (int space3 = height - 1; space3 > i; space3--)
+        for (int hash2 = 0; hash2 <= i; hash2++)
+        {
+            printf("#");
+        }
+         for (int space2 = height - 1; space2 > i; space2--)
         {
             printf(" ");
         }
+        for (int hash3 = 0; hash3 <= i; hash3++)
+        {
+            printf("#");
+        }
+        printf(" ");
 
+        for (int hash4 = 0; hash4 <= i; hash4++)
+        {
+            printf("#");
+        }
         printf("\n");
 
     }
