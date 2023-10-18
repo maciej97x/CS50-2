b79e41aa1364d94e374ec09fa3c8a9e3397950b8
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 07:36:13 +0000

    Wed, Oct 18, 2023, 7:36 AM +00:00

--- test.c
+++ test.c
@@ -8,7 +8,7 @@ int main(void)
     {
         height = get_int("Numbers of rows ");
     }
-    while (height > 1 || height < 9);
+    while (height < 1 || height > 9);
 
     for (int i = 0; i < height; i++)
     {
