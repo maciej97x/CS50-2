ed8efefd95dba0d16bc80e43256b200c1d4cee09
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 18:01:46 +0000

    Wed, Oct 18, 2023, 6:01 PM +00:00

---
+++ prim.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+  // TODO
+    if (number < 2)
+    {
+        return false;
+    }
+
+    int j;
+    for (j = 2; j <= number - 1; j++)
+    {
+        if (number %j == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
