19e47aba1b533d8576888646e592b5c26bda9b7d
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-18 08:23:02 +0000

    Wed, Oct 18, 2023, 8:23 AM +00:00

--- u.c
+++ u.c
@@ -37,30 +37,39 @@ int main(void)
 
 int get_cents(void)
 {
-    // TODO
-    return cents;
-}
 
+   int cents;
+
+   do
+   {
+    cents = get_int("Change Owed: ");
+   }
+   while (cents < 0);
+
+   return cents;
+
+
+}
 int calculate_quarters(int cents)
 {
-    // TODO
+    int quarters = cents / 25;
     return quarters;
 }
 
 int calculate_dimes(int cents)
 {
-    // TODO
+    int dimes = cents / 10;
     return dimes;
 }
 
 int calculate_nickels(int cents)
 {
-    // TODO
+    int nickels = cents / 5;
     return nickels;
 }
 
 int calculate_pennies(int cents)
 {
-    // TODO
+    int pennies = cents / 1;
     return pennies;
 }
