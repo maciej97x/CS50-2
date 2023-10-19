c8023b970de574318a6ee30f7becd02efeb63666
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-19 07:36:46 +0000

    Thu, Oct 19, 2023, 7:36 AM +00:00

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
 int get_cents(void);
 int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
-
 int main(void)
 {
     // Ask how many cents the customer is owed
