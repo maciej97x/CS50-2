67f7c1a1d19af138cb631a07ba416a66a993da58
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-20 09:11:40 +0000

    Fri, Oct 20, 2023, 9:11 AM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -8,7 +8,30 @@ void print_bulb(int bit);
 
 int main(void)
 {
-    // TODO
+	string message = get_string("Message: ");
+	int len = 0;
+	len  = strlen(message);
+
+	for (int i = 0; i < len; i++)
+	{
+		int binary[8] = {0};
+		int decimal = message[i];
+
+		int j = 0;
+		while (decimal > 0)
+		{
+			binary[j] = decimal % 2;
+			decimal /= 2;
+			j++;
+		}
+
+		for (int k = BITS_IN_BYTE - 1; k >= 0; k--)
+		{
+			print_bulb(binary[k]);
+		}
+		printf("\n");
+	}
+
 }
 
 void print_bulb(int bit)
