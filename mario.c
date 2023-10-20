3bf9019796b1b352eecfd7b5e63d9ded72995fa3
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-20 09:47:45 +0000

    Fri, Oct 20, 2023, 9:47 AM +00:00

--- mario.c
+++ mario.c
@@ -5,27 +5,12 @@ int main(void)
 {
     int height;
     do
-    {
-        height = get_int("Numbes of rows: ");
-    }
-    while (height < 2 || height > 8);
+    height = get_int("height ");
+    while(height < 2);
 
-    for (int i = 0; i < height; i++)
+    for(i = 0; i < height; i++);
     {
-        for (int spaces = height - 1; spaces > i; spaces--)
-        {
-            printf(" ");
-        }
-        for (int hashes = 0; hashes <= i; hashes++)
-        {
-            printf("#");
-        }
-        printf(" ");
-        for(int hashes2 = 0; hashes2 <= i; hashes2++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        for(HASH )
     }
 
 }
