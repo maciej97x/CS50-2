cfeb0c8e5498246b0ee62d38d4f581dba4778866
Maciej Sykuła <142264723+maciej97x@users.noreply.github.com>
2023-10-20 10:43:42 +0000

    Fri, Oct 20, 2023, 10:43 AM +00:00

--- population.c
+++ population.c
@@ -3,26 +3,16 @@
 
 int main(void)
 {
-  int start;
-  do
-  {
-    start = get_int("Start size: ");
-  }
-  while (start < 9);
+ int start;
+ do
+ start = get_int("Start ");
+ while (start < 2);
 
-  int end;
-  do  
-  {
-    end = get_int("End size: ");
-  }
-  while (end < start);
+int end;
+do
+end = get_int("End ");
+while (end > 8);
 
-  int years = 0;
-  do
-  {
-    start = start + start / 3 - start /4;
-    years++;
-  }
-  while (start < end);
-  printf("Years: %i\n", years);
+years = 0;
+for(int i = 0; in)
 }
